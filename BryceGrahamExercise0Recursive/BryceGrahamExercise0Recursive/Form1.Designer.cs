2da332d22c72ddad1f3f839ef6a142223fd1c61f
physeo <30989807+physeo@users.noreply.github.com>
2019-03-16 00:13:36 -0600

    Add files via upload

---
+++ BryceGrahamExercise0Recursive/BryceGrahamExercise0Recursive/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace BryceGrahamExercise0Recursive
+{
+    partial class Exercise0Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.stringBox1 = new System.Windows.Forms.TextBox();
+            this.stringBox2 = new System.Windows.Forms.TextBox();
+            this.interleaveButton = new System.Windows.Forms.Button();
+            this.displayBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sumStringDifferenceButton = new System.Windows.Forms.Button();
+            this.checkCharsButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // stringBox1
+            // 
+            this.stringBox1.Location = new System.Drawing.Point(90, 31);
+            this.stringBox1.Name = "stringBox1";
+            this.stringBox1.Size = new System.Drawing.Size(100, 20);
+            this.stringBox1.TabIndex = 0;
+            // 
+            // stringBox2
+            // 
+            this.stringBox2.Location = new System.Drawing.Point(90, 57);
+            this.stringBox2.Name = "stringBox2";
+            this.stringBox2.Size = new System.Drawing.Size(100, 20);
+            this.stringBox2.TabIndex = 1;
+            // 
+            // interleaveButton
+            // 
+            this.interleaveButton.Location = new System.Drawing.Point(90, 83);
+            this.interleaveButton.Name = "interleaveButton";
+            this.interleaveButton.Size = new System.Drawing.Size(109, 23);
+            this.interleaveButton.TabIndex = 2;
+            this.interleaveButton.Text = "Interleave Strings";
+            this.interleaveButton.UseVisualStyleBackColor = true;
+            this.interleaveButton.Click += new System.EventHandler(this.interleaveButton_Click);
+            // 
+            // displayBox
+            // 
+            this.displayBox.Location = new System.Drawing.Point(32, 138);
+            this.displayBox.Name = "displayBox";
+            this.displayBox.Size = new System.Drawing.Size(576, 96);
+            this.displayBox.TabIndex = 3;
+            this.displayBox.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "String 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "String 2";
+            // 
+            // sumStringDifferenceButton
+            // 
+            this.sumStringDifferenceButton.Location = new System.Drawing.Point(224, 83);
+            this.sumStringDifferenceButton.Name = "sumStringDifferenceButton";
+            this.sumStringDifferenceButton.Size = new System.Drawing.Size(127, 23);
+            this.sumStringDifferenceButton.TabIndex = 6;
+            this.sumStringDifferenceButton.Text = "Sum String Difference";
+            this.sumStringDifferenceButton.UseVisualStyleBackColor = true;
+            this.sumStringDifferenceButton.Click += new System.EventHandler(this.sumStringDifferenceButton_Click);
+            // 
+            // checkCharsButton
+            // 
+            this.checkCharsButton.Location = new System.Drawing.Point(389, 83);
+            this.checkCharsButton.Name = "checkCharsButton";
+            this.checkCharsButton.Size = new System.Drawing.Size(123, 23);
+            this.checkCharsButton.TabIndex = 7;
+            this.checkCharsButton.Text = "Check Characters";
+            this.checkCharsButton.UseVisualStyleBackColor = true;
+            this.checkCharsButton.Click += new System.EventHandler(this.checkCharsButton_Click);
+            // 
+            // Exercise0Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 261);
+            this.Controls.Add(this.checkCharsButton);
+            this.Controls.Add(this.sumStringDifferenceButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.displayBox);
+            this.Controls.Add(this.interleaveButton);
+            this.Controls.Add(this.stringBox2);
+            this.Controls.Add(this.stringBox1);
+            this.Name = "Exercise0Form";
+            this.Text = "Exercise 0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox stringBox1;
+        private System.Windows.Forms.TextBox stringBox2;
+        private System.Windows.Forms.Button interleaveButton;
+        private System.Windows.Forms.RichTextBox displayBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button sumStringDifferenceButton;
+        private System.Windows.Forms.Button checkCharsButton;
+    }
+}
+
---
+++ BryceGrahamExercise0Recursive/BryceGrahamExercise0Recursive/Form1.cs
@@ -0,0 +1,121 @@
+﻿/**********************************************
+Bryce Graham
+CS 2282
+Dr. Beard
+Exercise 0: An introductory program to learn how to use recursion.
+**********************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BryceGrahamExercise0Recursive
+{
+    public partial class Exercise0Form : Form
+    {
+        public Exercise0Form()
+        {
+            InitializeComponent();
+        }
+
+        // Calls the interleave strings method after obtaining the strings 
+        // from the textboxes on the form then displays the results.
+        private void interleaveButton_Click(object sender, EventArgs e)
+        {
+            string s1 = stringBox1.Text.Trim();
+            string s2 = stringBox2.Text.Trim();
+            string newString = interleaveStrings(s1, s2, "", 0);
+            displayBox.Text = newString;
+        }
+
+        // Calls the sumStringDif method after obtaining the strings
+        // from the textboxes on the form then displays the results.
+        private void sumStringDifferenceButton_Click(object sender, EventArgs e)
+        {
+            string s1 = stringBox1.Text.Trim();
+            string s2 = stringBox2.Text.Trim();
+            int asciiSum = sumStringDif(s1, s2, 0, 0);
+            displayBox.Text = "The sum of the differences is: " + asciiSum;
+        }
+
+        // Calls the checkChars method after obtaining the strings
+        // frmo the textboxes on the form then displays the results.
+        private void checkCharsButton_Click(object sender, EventArgs e)
+        {
+            string s1 = stringBox1.Text.Trim();
+            string s2 = stringBox2.Text.Trim();
+            bool charInBoth = checkChars(s1, s2, 0, false);
+            if(charInBoth)
+            {
+                displayBox.Text = "The strings both have the same characters.";
+            }
+            else
+            {
+                displayBox.Text = "The strings do not have the same characters.";
+            }
+        }
+
+        // Combines two strings in an interleaving pattern so that the first
+        // character of string one is followed by the first character of string
+        // two and so on.
+        public string interleaveStrings(string s1, string s2, string newString, int count)
+        {
+            if (count < s1.Length && count < s2.Length)
+            {
+                newString += s1[count].ToString() + s2[count].ToString();
+                count++;
+                newString = interleaveStrings(s1, s2, newString, count);
+            }
+            else if (count == s1.Length && count != s2.Length)
+            {
+                newString += s2.Substring(count);
+            }
+            if (count == s2.Length && count != s1.Length)
+            {
+                newString += s1.Substring(count);
+            }
+
+            return newString;
+        }
+
+        // Obtains the sum of the differences of corresponding ascii
+        // characters in two equal length strings.
+        public int sumStringDif(string s1, string s2, int count, int asciiSum)
+        {
+            if (s1.Length != s2.Length)
+                return -1;
+            else if (count < s1.Length)
+            {
+                asciiSum += (int)s1[count] - (int)s2[count];
+                count++;
+                asciiSum = sumStringDif(s1, s2, count, asciiSum);
+            }
+            return asciiSum;
+        }
+
+        // Checks to see if all of the characters in string one are in
+        // string two and vice versa.
+        public bool checkChars(string s1, string s2, int count, bool charInBoth)
+        {
+            if (s1.Length >= s2.Length && count < s1.Length)
+            {
+                charInBoth = s2.IndexOf(s1[count]) > -1;
+                count++;
+                charInBoth = checkChars(s1, s2, count, charInBoth);
+            }
+            else if (s2.Length >= s1.Length && count < s2.Length)
+            {
+                charInBoth = s1.IndexOf(s2[count]) > -1;
+                count++;
+                charInBoth = checkChars(s1, s2, count, charInBoth);
+            }
+            return charInBoth;
+        }
+    }
+}
